c6845dc3ce83fc7ce71885663c69831015b77121
COREDs <headgic@gmail.com>
2019-06-12 23:04:48 +0700

    Publish

---
+++ build/precache-manifest.b1cbaa5e9f5cc7971bfe8af418ce6e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3a4e6c8d10b085feb15c3651b6eaf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4eb2106c94152080325",
+    "url": "/static/css/2.b0f64131.chunk.css"
+  },
+  {
+    "revision": "64943da9665c49907bab",
+    "url": "/static/css/main.a79f424e.chunk.css"
+  },
+  {
+    "revision": "a4eb2106c94152080325",
+    "url": "/static/js/2.72e47eb4.chunk.js"
+  },
+  {
+    "revision": "64943da9665c49907bab",
+    "url": "/static/js/main.9d7a588a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
